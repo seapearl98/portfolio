68dccd116329e9e28f0456e5aca2e4aeb21581a1
seapearl98 <gown1977@gmail.com>
2022-11-18 10:23:27 +0900

    three

--- src/pages/ProjectNav.js
+++ src/pages/ProjectNav.js
@@ -8,19 +8,19 @@ function ProjectNav({IoMdRefresh}) {
         <ul className='navbox'>
           <li className='nav0 on'>
               <span>CJONE</span>
-              <img src="../img/blink.png" alt="반짝이" />
+              <img src="./img/blink.png" alt="반짝이" />
           </li>
           <li className='nav1'>
               <span>ATWOM</span>
-              <img src="../img/blink.png" alt="반짝이" />
+              <img src="./img/blink.png" alt="반짝이" />
           </li>
           <li className='nav2'>
               <span>KA-TALK</span>
-              <img src="../img/blink.png" alt="반짝이" />
+              <img src="./img/blink.png" alt="반짝이" />
           </li>
           <li className='nav3'>
             <span>NETFLIX</span>
-            <img src="../img/blink.png" alt="반짝이" />
+            <img src="./img/blink.png" alt="반짝이" />
           </li>
         </ul>
         <span><Link to='/'><IoMdRefresh/></Link></span>
