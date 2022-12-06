168389f39ae7bd19fbff57847ad78c307e3006bb
seapearl98 <gown1977@gmail.com>
2022-12-06 10:03:14 +0900

    two

--- js/main.js
+++ js/main.js
@@ -47,3 +47,106 @@ function toTop(){
   });
 }
 self.setTimeout("toTop()",9500);
+
+
+const projects = document.querySelectorAll('section');
+  console.log(projects);
+
+  const projectNav = document.querySelectorAll(".navi");
+  console.log(projectNav,"네비게이션")
+
+
+for(let i=0;i<projectNav.length; i++){
+  projectNav[i].addEventListener("click",e=>{
+    e.preventDefault();
+  for(let el of projectNav){
+    el.classList.remove('on')
+  };
+  for(let el of projects){
+    el.classList.remove('on')
+  };
+    projectNav[i].classList.add('on');
+    projects[i].classList.add('on');
+  })
+} 
+
+//스크롤
+let devHeight;
+devHeight = window.innerHeight;
+console.log(devHeight,"현재높이값")
+window.addEventListener('resize',()=>{
+    devHeight = window.innerHeight;
+    console.log(devHeight) //현재높이값
+})
+const sections = document.querySelectorAll("section");
+console.log(sections,"페이지 수")
+for(let i=0;i<sections.length;i++){
+    sections[i].style.height = devHeight + 'px';
+}
+
+
+window.addEventListener("scroll",e=>{
+    let scroll = document.querySelector("html").scrollTop;
+    console.log(scroll,"스크롤값")
+    for(let i=0;i<sections.length;i++){
+        if(scroll>=(i*devHeight) && scroll<(i+1)*devHeight){
+            activation(i,sections)
+        }   
+    }
+})
+let activation =(index,sections)=>{
+    for(let el of sections){
+        el.classList.remove("on")
+    }
+    sections[index].classList.add("on")
+}
+
+
+
+
+window.addEventListener("scroll", ()=>{
+
+    let scroll = document.querySelector("html").scrollTop;
+    
+    for(let i=0; i<sections.length; i++){
+        if(scroll >= i*devHeight && scroll < (i+1)*devHeight){
+            activation(i,sections);
+        }
+    }
+
+
+    let contents = document.querySelectorAll(".Page>section")
+    for(let i=0; i<contents.length; i++){
+        contents[i].addEventListener("wheel", e=>{
+            if(e.wheelDelta >= 0){
+                let prev = e.currentTarget.previousElementSibling.offsetTop;
+                window.scroll({
+                    top: prev,
+                    left: 0,
+                    behavior: "smooth"
+                    
+                });
+            }else if(e.wheelDelta <= 0){
+                let next = e.currentTarget.nextElementSibling.offsetTop;
+                window.scroll({
+                    top: next,
+                    left: 0,
+                    behavior: "smooth"
+                });
+            }
+        });
+    };
+})
+
+//navigation버튼 클릭시 해당페이지로 이동
+
+for(let k=0;k<projectNav.length;k++){
+    projectNav[k].addEventListener("click",e=>{
+        e.preventDefault();
+        window.scroll({
+            top:(k+2)*devHeight,
+            left:0,
+            behavior:'smooth'
+        })
+    })
+}
